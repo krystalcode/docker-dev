8e36509f453e4be378f8412403cbe08811a67d34
Dimitris Bozelos <dbozelos@gmail.com>
2025-01-22 12:35:06 +0000

    f_lagoon_cli Added project to alias via environment variable

--- fedora/lagoon-cli/0/.bashrc.d/lagoon.aliases.sh
+++ fedora/lagoon-cli/0/.bashrc.d/lagoon.aliases.sh
@@ -1,2 +1,2 @@
 # Run Lagoon with all required options pre-filled.
-alias l='lagoon --config-file /src/.lagoon.yml --ssh-key /root/.ssh/id_ed25519'
+alias l='lagoon --config-file /src/.lagoon.yml --ssh-key /root/.ssh/id_ed25519 --project "${LAGOON_PROJECT}"'
