846bed12a864ba9d9d06c62db898bfdf01a30ca4
Dimitris Bozelos <dbozelos@gmail.com>
2025-04-16 03:51:15 +0000

    f_nvm Moved Bash configuration to a separate file

---
+++ fedora/node.js/nvm/.bashrc.d/nvm.sh
@@ -0,0 +1,4 @@
+# Exact copy of the entries added to `.bashrc` by the installer.
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
