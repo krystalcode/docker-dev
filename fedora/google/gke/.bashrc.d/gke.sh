20727eb03d26ab96e9685e5d7941b7a032be86c9
Dimitris Bozelos <dbozelos@gmail.com>
2025-04-17 02:38:17 +0000

    f_google_gke Authenticate if not already logged in

--- fedora/google/gke/.bashrc.d/gke.sh
+++ fedora/google/gke/.bashrc.d/gke.sh
@@ -1,2 +1,9 @@
 # Run `gcloud` with all required options pre-filled.
 alias g='gcloud --project ${GCLOUD_PROJECT} --account ${GCLOUD_ACCOUNT}'
+
+if g projects list &> /dev/null; then
+    echo "Logged as as ${GCLOUD_ACCOUNT}"
+else
+    g auth login
+    g auth list
+fi
