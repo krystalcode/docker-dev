1b770342df5b65d0a6f48f2b507a161a855257c6
Dimitris Bozelos <dbozelos@gmail.com>
2025-06-12 02:12:46 +0000

    f_fedora/d_debian Do not assume the path to the home directory

--- fedora/fedora/.bashrc.d/oh-my-posh.sh
+++ fedora/fedora/.bashrc.d/oh-my-posh.sh
@@ -1,4 +1,4 @@
 # Configure the Bash shell to use `oh-my-posh` by default.
 if [ -f `which oh-my-posh` ]; then
-    eval "$(oh-my-posh init bash --config /root/.config/oh-my-posh/themes/runnah.minimal.omp.json)"
+    eval "$(oh-my-posh init bash --config /${HOME}/.config/oh-my-posh/themes/runnah.minimal.omp.json)"
 fi
