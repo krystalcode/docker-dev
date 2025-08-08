03a95275ef71aeacfe85f45b2a0e15ecb2afd0c2
Dimitris Bozelos <dbozelos@gmail.com>
2025-08-08 17:16:17 +0000

    lsp_tabby_server Support downloading models via entrypoint

--- language-servers/tabby/server/entrypoint.sh
+++ language-servers/tabby/server/entrypoint.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
+    printf "[ERROR] Please provide the action\n"
+    exit
+fi
+
+action=$1
+
+if [ $# -eq 1 ]; then
     printf "[ERROR] Please provide the model\n"
     exit
 fi
 
-model=$1
+model=$2
 
-tabby download --model "${model}"
+case "${action}" in
+    download)
+        tabby download --model "${model}"
+        ;;
 
-if [ $# -eq 1 ]; then
-    tabby serve --mode "${model}"
-else
-    tabby serve --mode "${model}" --chat-model "$2"
-fi
+    run)
+        if [ $# -eq 2 ]; then
+            tabby serve --model "${model}"
+        else
+            chat_model=$3
+            tabby serve --model "${model}" --chat-model "${chat_model}"
+        fi
+        ;;
+
+    *)
+        printf "[ERROR] Unsupported action ${action}\n"
+        exit
+        ;;
+esac
