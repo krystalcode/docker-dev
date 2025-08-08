bf80296ad118206f1d9162d1708181bad732d752
Dimitris Bozelos <dbozelos@gmail.com>
2025-08-08 19:48:17 +0000

    lsp_tabby_agent Added image for TabbyML LSP agent

---
+++ language-servers/tabby/agent/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. /root/.bashrc
+
+if [ $# -eq 0 ]; then
+    echo "Please provide the port to expose the language server on"
+    exit
+fi
+
+port=$1
+
+export LANG=C.UTF-8
+
+socat tcp-listen:"$port",reuseaddr exec:"npx tabby-agent --stdio"
